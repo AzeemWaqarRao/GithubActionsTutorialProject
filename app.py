c432eff208d97008df436f4ee46741f58c5e12c7
AzeemWaqarRao <azeemwaqar2k15@gmail.com>
2024-11-12 02:08:51 +0500

    display message updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return "The change works!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
